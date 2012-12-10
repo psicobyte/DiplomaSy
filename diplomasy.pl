f81140652c430b28dbfdfb336a1711bf4218b7cf
psicobyte <psicobyte@gmail.com>
2012-12-10 18:35:07 +0100

    Script en perl para imprimir lotes de diplomas usando plantillas en pdf y listados csv

---
+++ diplomasy.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+use PDF::Reuse;
+
+# Default values
+my $FileConfig= 'config.txt';
+my $InputPDF= 'model.pdf';
+my $OutputPDF= 'diplomas.pdf';
+my $DataFile= 'people.csv';
+my $Font= 'Times-Bold';
+my $FontSize= 40;
+
+
+
+
+#Open config file
+
+open (CONFIG, "<$FileConfig") || print "Config no está";
+
+while (<CONFIG>) {
+	chomp;
+    if (/^#/ || $_ eq "") {
+		#print "Ignored\n";
+	}
+    else {
+		if (s/^COORDINATES=//){@Coords= split /[\.\,]/;}
+		if (s/^OutputPDF=//){$OutputPDF= $_;}		
+		if (s/^InputPDF=//){$InputPDF= $_;}
+		if (s/^DataFile=//){$DataFile= $_;}	
+	}    
+}
+close CONFIG;
+
+prFile($OutputPDF) || die "No he podido abrir fichero $InputPDF\n";
+
+prFontSize($FontSize);                      
+prFont($Font); 
+
+#Open data file
+
+open (DATA, "<$DataFile") || print "DATA no está: $DataFile \n";
+
+while (<DATA>) {
+	chomp;
+	@Cell= split /\,/;
+	my @plantilla= prForm($InputPDF);
+	#Para quitar el primer elemento, buscar algo más elegante
+	my $quita = shift @plantilla;
+	prMbox(@plantilla); 
+	$i=0;
+	foreach (@Cell){
+		prText($Coords[$i*2], $Coords[($i*2)+1], $Cell[$i], 'left', 90);
+		$i++;
+	}
+	prPage();
+}
+close DATA;
+
+
+ 
+  
+prEnd();  
+  
+
